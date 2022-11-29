4eccf2b2b094470d2dfe4b240799512e0075a1d3
ripa1995 <roberto.ripamonti@ymail.com>
2022-11-29 13:34:09 +0100

    feat(wip): new expense form

--- expenses/src/components/Expenses/Expenses.js
+++ expenses/src/components/Expenses/Expenses.js
@@ -6,14 +6,17 @@ const Expenses = (props) => {
   return (
     <Card className="expenses">
       {props.expenses.map((expense) => {
-        return <ExpenseItem
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />;
+        return (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        );
       })}
     </Card>
   );
-}
+};
 
 export default Expenses;
